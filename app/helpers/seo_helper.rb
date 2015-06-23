ccff1b02b3a9022f0fc46578f32f943e4f7c3a57
Eovy Futureworkz <eovy@futureworkz.com>
2015-06-23 10:36:48 +0700

    Update database & Add meta tag

--- app/helpers/seo_helper.rb
+++ app/helpers/seo_helper.rb
@@ -6,28 +6,29 @@ module SeoHelper
 									:description 		=> "All text about keywords, other keywords", 		# Recommended up to 160 characters
 									:canonical 			=> "http://localhost.com:3000",										# Main URL for a content
 									:alternate 			=> { "en" => "http://localhost.com:3000/en",			# Enable this attribute if your site has many language
-	                              			 "vi" => "http://localhost.com:3000/vi" },
-	                # :noindex 			=> true,
+																			 "vi" => "http://localhost.com:3000/vi" },
+									:viewport				=> "width=device-width, initial-scale=1.0",
+									# :noindex 			=> true,
 									# :nofollow 		=> true,
-	                :og => {  																												# Share your link easier on Facebook
-									  :site_name 		=> "Project (Like :site attribute)",
-									  :title 				=> "article/purpose this page",
-									  :type 				=> "objec/photo/article/summary/etc.",
-									  :image 				=> "link thumbnail image for review",
-									  :url 					=> "current link",
-									  :description 	=> "Intro/short description",
+									:og => {  																												# Share your link easier on Facebook
+										:site_name 		=> "Project (Like :site attribute)",
+										:title 				=> "article/purpose this page",
+										:type 				=> "objec/photo/article/summary/etc.",
+										:image 				=> "link thumbnail image for review",
+										:url 					=> "current link",
+										:description 	=> "Intro/short description",
 									},
 
 									:twitter => {  																			# Share your link easier on twitter
 										:site  				=> "Project (Like :site attribute)",
 										:title 				=> "article/purpose this page",
-									  :card  				=> "objec/photo/article/summary/etc.",
-									  :description  => "Intro/short description",
-									  :image => {
-									    :src    => "link thumbnail image for review",
-									    :width  => 300,
-									    :height => 300
-									  }
+										:card  				=> "objec/photo/article/summary/etc.",
+										:description  => "Intro/short description",
+										:image => {
+											:src    => "link thumbnail image for review",
+											:width  => 300,
+											:height => 300
+										}
 									}
 		end
 end
